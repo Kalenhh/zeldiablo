0d3b8eeaf0b072de4dd68cc5cca430f48e2e0a0d
Marimbanath <nathan.eyer52700@gmail.com>
2024-06-05 14:13:24 +0200

    Ajout de ScoreJeu

---
+++ data/src/ScoreJeu.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class ScoreJeu {
+    private String nomFichier;
+
+    FileWriter fw;
+
+    public ScoreJeu(String nomFichier) throws IOException {
+        this.nomFichier = nomFichier;
+        this.fw = new FileWriter(nomFichier);
+    }
+
+    public void ajouterScore(String pseudo, int score) throws IOException {
+        this.fw.write(pseudo + ";" + score + ";\r");
+    }
+
+    public void fermerFichier() throws IOException {
+        this.fw.close();
+    }
+
+    public String getMeilleurScore() throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader(this.nomFichier));
+        int meilleurScore = 0;
+        String ligne;
+        while(true){
+            try{
+                ligne = br.readLine();
+                String[] ligneSepare = ligne.split(";");
+                int scoreCourant = Integer.parseInt(ligneSepare[1]);
+                if(scoreCourant > meilleurScore)
+                    meilleurScore = scoreCourant;
+            }catch (EOFException e){
+                break;
+            }catch (IOException e){
+                break;
+            }
+        }
+        String scoreMax = String.valueOf(meilleurScore);
+        return scoreMax;
+    }
+
+
+}
