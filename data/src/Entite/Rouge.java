bcb716ed9622327bdb594f64cfa9ab829ae16fbf
Drouvot Melody <melody.drouvot@gmail.com>
2024-06-11 10:18:30 +0200

    Ajout seDeplacer rouge (non fonctionnel)

--- data/src/Entite/Rouge.java
+++ data/src/Entite/Rouge.java
@@ -1,9 +1,18 @@
 package Entite;
 
+import Algorithme.Dijkstra;
+import Algorithme.Valeur;
+import Labyrinthe.Labyrinthe;
+
+import java.util.ArrayList;
+
 /**
  * Classe représentant un monstre Rouge, qui hérite de Entite.Monstre.
  */
 public class Rouge extends Monstre {
+
+    Labyrinthe l;
+    Dijkstra dj = new Dijkstra();
 
     /**
      * Constructeur pour initialiser un monstre Rouge avec une position, des points de vie, et des dégâts.
@@ -13,8 +22,9 @@ public class Rouge extends Monstre {
      * @param pv     Points de vie initiaux
      * @param degats Dégâts infligés par le monstre
      */
-    public Rouge(int dx, int dy, int pv, int degats) {
+    public Rouge(int dx, int dy, int pv, int degats, Labyrinthe l) {
         super(dx, dy, pv, degats, null);
+        this.l = l;
     }
 
     /**
@@ -30,8 +40,23 @@ public class Rouge extends Monstre {
      * Se déplace de sorte à se diriger constamment vers le joueur.
      * Implémentation à définir selon la logique de déplacement spécifique.
      */
-    public void sedeplacer() {
-        // se déplace de sorte à se déplacer constamment vers le joueur
+    public void seDeplacer() {
+        //se deplace de sorte a ce qu'il se déplace comstamment vers le joueur
+        int x = this.getX();
+        int y = this.getY();
+        Valeur v = this.dj.resoudre(this.l, x+":"+y);
+        ArrayList<String> chemin = (ArrayList<String>) v.calculerChemin(l.getPerso().getX() + ":" + l.getPerso().getY());
+        System.out.println("size : " + chemin.size());
+        String[] position = chemin.get(0).split(":");
+        System.out.println("Chemin pris : " + chemin);
+        ArrayList<int[]> liste = new ArrayList<int[]>();
+
+        int[] nouvellePosition = {Integer.parseInt(position[0]), Integer.parseInt(position[1])};
+        liste.add(nouvellePosition);
+        if(liste.isEmpty()){
+            this.setX(nouvellePosition[0]);
+            this.setY(nouvellePosition[1]);
+        }
     }
 
     /**
